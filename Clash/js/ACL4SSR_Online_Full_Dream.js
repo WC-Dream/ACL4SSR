f30d23f1fee7e9ec0a79a747c7605314338b1579
WC-Dream <103100067+WC-Dream@users.noreply.github.com>
2025-09-29 13:41:55 +0800

    [add]

--- Clash/js/ACL4SSR_Online_Full_Dream.js
+++ Clash/js/ACL4SSR_Online_Full_Dream.js
@@ -1,7 +1,7 @@
 async function main(config) {
 
     config["disable-keep-alive"] = false;
-    config["keep-alive-idle"] = 90;
+    config["keep-alive-idle"] = 600;
     config["keep-alive-interval"] = 75;
     config["tcp-concurrent"] = false;
 
